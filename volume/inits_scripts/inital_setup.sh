c8e4ffe517f840b112274d46819b1879a8d6f46e
Manojkumar Subramaniam <manojs@efinixinc.com>
2023-03-02 13:38:32 +0800

    fixed CR issue in scripts

--- volume/inits_scripts/inital_setup.sh
+++ volume/inits_scripts/inital_setup.sh
@@ -1,5 +1,5 @@
-sudo -- sh -c 'chgrp -R user /app/zephyrrtos && chmod -R g+rwx /app/zephyrrtos';
+sudo -- sh -c "chgrp -R user /app/zephyrrtos && chmod -R g+rwx /app/zephyrrtos"
 touch zephyr_efx_setup.sh && echo "west init -m git@github.com:manoj153/ps_can_bus_prj_r.git --mr wip/efinix_add_soc && west update \
-&& echo -e \"\e[32m{cd zephyr} -- to work on zephyrRTOS \e[0m\n\" " \
+&& echo -e \"\e[32m{cd zephyr} -- to work on zephyrRTOS \e[0m\n\"" \
 > zephyr_efx_setup.sh && chmod +x zephyr_efx_setup.sh && \
 echo -e "\e[32mRun ./zephyr_efx_setup.sh - to setup zephyr repo\e[0m\n"
