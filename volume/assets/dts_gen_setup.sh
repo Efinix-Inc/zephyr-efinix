38cdd15762d742b025a952780175633c2e8c6511
jjho <jjho@efinixinc.com>
2023-08-30 13:19:39 +0800

    Update the dt-generator repo to official repo.
    Signed-off-by: jjho <jjho@efinixinc.com>

--- volume/assets/dts_gen_setup.sh
+++ volume/assets/dts_gen_setup.sh
@@ -1,4 +1,4 @@
-git clone -b zephyr_installer https://github.com/joo-jie/dt-generator.git # Use https://github.com/Efinix-Inc/dt-generator for official release
+git clone https://github.com/Efinix-Inc/dt-generator
 
 
 
